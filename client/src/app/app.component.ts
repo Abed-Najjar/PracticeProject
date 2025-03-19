276d6b9ad56f0cefccb1cf3ccfb9776b36a74715
Abed-Najjar <abdnajjar082@gmail.com>
2025-03-19 20:17:36 +0300

    first commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavComponent, HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent implements OnInit{
+  
+  private accountService = inject(AccountService);
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.CurrentUser.set(user);
+  }
+
+
+  
+  
+}
