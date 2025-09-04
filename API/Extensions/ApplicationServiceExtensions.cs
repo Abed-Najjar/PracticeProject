d522ef2bdd25d74a5625a868c56066ddebdac9a8
Abed-Najjar <abdnajjar082@gmail.com>
2025-09-04 11:51:04 +0300

    Configured swagger and added a couple of users

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -15,6 +15,43 @@ public static class ApplicationServiceExtensions
          IConfiguration config)
   {
     services.AddControllers();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo 
+        { 
+            Title = "Dating App API", 
+            Version = "v1" 
+        });
+        
+        // Add JWT Authentication
+        c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
+            Name = "Authorization",
+            In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+            Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+        {
+            {
+                new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                    {
+                        Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                },
+                new List<string>()
+            }
+        });
+    });
     services.AddDbContext<DataContext>(opt =>
     {
 
--- API/Program.cs
+++ API/Program.cs
@@ -19,6 +19,12 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
@@ -48,5 +54,8 @@ catch (Exception ex)
    var logger = services.GetRequiredService<ILogger<Program>>();
    logger.LogError(ex, "An error occured during migration");
 }
+
+// Add a simple root endpoint
+app.MapGet("/", () => "Dating App API is running!");
 
 app.Run();
